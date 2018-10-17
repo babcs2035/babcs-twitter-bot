8b9387ce5cea25ceca29765ad158b740e2ab28f7
Bwambocos <mijyumaru2525@outlook.jp>
2018-10-17 21:01:45 +0900

    announce: ツイート文言変更

--- announce.py
+++ announce.py
@@ -5,8 +5,7 @@ import datetime
 import random
 
 announceMessages = ["この Bot の名前を募集中です．リプライ・DM にて受付中です！",
-    "この Bot は開発中です．AtCoder Problems の API を使用する予定です．何かご意見がありましたら，リプライ・DM にて受付中です！",
-    "この Bot の作者は Bwambocos ( @babcs2035 ) です．意見・不満・批判はこちらへお願いします！"]
+    "この Bot は開発中です．何かご意見がありましたら，リプライ・DM にて受付中です！"]
 
 def announce():
 
