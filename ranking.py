fc7e9ee87bca93fb7070a5ef3f81ecdcf50848eb
Bwambocos <mijyumaru2525@outlook.jp>
2018-10-20 16:10:53 +0900

    ranking: Point Sum ランキングを実装

--- ranking.py
+++ ranking.py
@@ -11,6 +11,7 @@ from PIL import Image, ImageDraw, ImageFont
 AtCoderID = []
 TwitterID = []
 acCount = []
+acPoint = []
 
 # Dropbox からダウンロード
 def downloadFromDropbox():
@@ -19,6 +20,7 @@ def downloadFromDropbox():
     global AtCoderID
     global TwitterID
     global acCount
+    global acPoint
 
     # Dropbox オブジェクトの生成
     dbx = dropbox.Dropbox(os.environ["DROPBOX_KEY"])
@@ -39,7 +41,7 @@ def downloadFromDropbox():
         for id in f:
             TwitterID.append(id.rstrip("\n"))
     print("ranking: Downloaded TwitterID (size : ", str(len(TwitterID)), ")")
-
+    
     # acCount をダウンロード
     dbx.files_download_to_file("acCount.txt", "/acCount.txt")
     with open("acCount.txt", "r") as f:
@@ -48,11 +50,20 @@ def downloadFromDropbox():
             acCount.append(int(acs.rstrip("\n")))
     print("ranking: Downloaded acCount (size : ", str(len(acCount)), ")")
 
+    # acPoint をダウンロード
+    dbx.files_download_to_file("acPoint.txt", "/acPoint.txt")
+    with open("acPoint.txt", "r") as f:
+        acPoint.clear()
+        for acs in f:
+            acPoint.append(int(acs.rstrip("\n")))
+    print("ranking: Downloaded acPoint (size : ", str(len(acPoint)), ")")
+
 # Dropbox にアップロード
 def uploadToDropbox():
     
     # グローバル変数
     global acCount
+    global acPoint
     
     # Dropbox オブジェクトの生成
     dbx = dropbox.Dropbox(os.environ["DROPBOX_KEY"])
@@ -67,10 +78,20 @@ def uploadToDropbox():
         dbx.files_upload(f.read(), "/acCount.txt")
     print("ranking: Uploaded acCount (size : ", str(len(acCount)), ")")
 
+    # acPoint をアップロード
+    with open("acPoint.txt", "w") as f:
+        for acs in acPoint:
+            f.write(str(acs) + "\n")
+    with open("acPoint.txt", "rb") as f:
+        dbx.files_delete("/acPoint.txt")
+        dbx.files_upload(f.read(), "/acPoint.txt")
+    print("ranking: Uploaded acPoint (size : ", str(len(acPoint)), ")")
+
 def ranking():
     
     # グローバル変数
     global acCount
+    global acPoint
 
     # 各種キー設定
     CK = os.environ["CONSUMER_KEY"]
@@ -88,47 +109,86 @@ def ranking():
 
     # AC 数を取得
     acCountJson = urllib.request.urlopen("https://kenkoooo.com/atcoder/atcoder-api/info/ac")
+    acPointJson = urllib.request.urlopen("https://kenkoooo.com/atcoder/atcoder-api/info/sums")
     acCountData = json.loads(acCountJson.read().decode("utf-8"))
+    acPointData = json.loads(acPointJson.read().decode("utf-8"))
     resCount = []
+    resPoint = []
     for user in acCountData:
         if user["user_id"] in AtCoderID:
             resCount.append(({"user_id" : str(user["user_id"]), "count" : int(user["problem_count"])}))
-    nowACCount = []   
+    for user in acPointData:
+        if user["user_id"] in AtCoderID:
+            resPoint.append(({"user_id" : str(user["user_id"]), "point" : int(user["point_sum"])}))
+    nowACCount = []
+    nowACPoint = []
     for id in AtCoderID:
         for user in resCount:
             if id == user["user_id"]:
                 nowACCount.append(user["count"])
                 break
+    for id in AtCoderID:
+        for user in resPoint:
+            if id == user["user_id"]:
+                nowACPoint.append(user["point"])
+                break
     newACCount = []
+    newACPoint = []
     for idx in range(len(acCount)):
         if nowACCount[idx] - acCount[idx] > 0:
             newACCount.append(({"user_id" : AtCoderID[idx], "count" : nowACCount[idx] - acCount[idx]}))
+    for idx in range(len(acPoint)):
+        if nowACPoint[idx] - acPoint[idx] > 0:
+            newACPoint.append(({"user_id" : AtCoderID[idx], "point" : nowACPoint[idx] - acPoint[idx]}))
     newACCount.sort(key = lambda x: x["count"], reverse = True)
+    newACPoint.sort(key = lambda x: x["point"], reverse = True)
 
-    # ランキングを作成
-    rankNum = 1
+    # Unique AC 数ランキングを作成
+    countRankNum = 1
     countNum = 1
     rankingFont = ImageFont.truetype("data/YuGothM.ttc", 32)
-    rankingFirstImg = Image.open("data/rankingImg (first).jpg")
-    resImg = Image.new("RGB", (738, 65 + 63 * len(newACCount)))
-    resImg.paste(rankingFirstImg, (0, 0))
+    countRankingFirstImg = Image.open("data/countRankingImg (first).jpg")
+    countResImg = Image.new("RGB", (738, 65 + 63 * len(newACCount)))
+    countResImg.paste(countRankingFirstImg, (0, 0))
     for idx in range(len(newACCount)):
-        rankingImg = Image.open("data/rankingImg (cell).jpg")
-        rankingDraw = ImageDraw.Draw(rankingImg)
+        countRankingImg = Image.open("data/rankingImg (cell).jpg")
+        countRankingDraw = ImageDraw.Draw(countRankingImg)
         if idx > 0:
             if int(newACCount[idx - 1]["count"]) > int(newACCount[idx]["count"]):
-                rankNum = rankNum + countNum
+                countRankNum = countRankNum + countNum
                 countNum = 1
             else:
                 countNum = countNum + 1
-        rankingDraw.text((10, 19), str(rankNum), fill = (0, 0, 0), font = rankingFont)
-        rankingDraw.text((120, 19), newACCount[idx]["user_id"], fill = (0, 0, 0), font = rankingFont)
-        rankingDraw.text((560, 19), str(newACCount[idx]["count"]), fill = (0, 0, 0), font = rankingFont)
-        resImg.paste(rankingImg, (0, 65 + 63 * idx))
-    resImg.save("data/rankingImg_fixed.jpg")
+        countRankingDraw.text((10, 19), str(countRankNum), fill = (0, 0, 0), font = rankingFont)
+        countRankingDraw.text((120, 19), newACCount[idx]["user_id"], fill = (0, 0, 0), font = rankingFont)
+        countRankingDraw.text((560, 19), str(newACCount[idx]["count"]), fill = (0, 0, 0), font = rankingFont)
+        countResImg.paste(countRankingImg, (0, 65 + 63 * idx))
+    countResImg.save("data/countRankingImg_fixed.jpg")
+    
+    # Point Sum ランキングを作成
+    pointRankNum = 1
+    pointNum = 1
+    pointRankingFirstImg = Image.open("data/pointRankingImg (first).jpg")
+    pointResImg = Image.new("RGB", (738, 65 + 63 * len(newACPoint)))
+    pointResImg.paste(pointRankingFirstImg, (0, 0))
+    for idx in range(len(newACPoint)):
+        pointRankingImg = Image.open("data/rankingImg (cell).jpg")
+        pointRankingDraw = ImageDraw.Draw(pointRankingImg)
+        if idx > 0:
+            if int(newACPoint[idx - 1]["point"]) > int(newACPoint[idx]["point"]):
+                pointRankNum = pointRankNum + pointNum
+                pointNum = 1
+            else:
+                pointNum = pointNum + 1
+        pointRankingDraw.text((10, 19), str(pointRankNum), fill = (0, 0, 0), font = rankingFont)
+        pointRankingDraw.text((120, 19), newACPoint[idx]["user_id"], fill = (0, 0, 0), font = rankingFont)
+        pointRankingDraw.text((560, 19), str(newACPoint[idx]["point"]), fill = (0, 0, 0), font = rankingFont)
+        pointResImg.paste(pointRankingImg, (0, 65 + 63 * idx))
+    pointResImg.save("data/pointRankingImg_fixed.jpg")
 
     # データをアップロード
     acCount = nowACCount
+    acPoint = nowACPoint
     uploadToDropbox()
 
     # 時刻表示を作成
@@ -136,5 +196,7 @@ def ranking():
     timeStamp = str(timeStamp.strftime("%Y/%m/%d %H:%M"))
 
     # ランキングをツイート
-    tweetText = "Unique AC 数ランキング TOP " + str(rankNum) + " !!\n"
-    api.update_with_media(filename = "data/rankingImg_fixed.jpg", status = tweetText + "\n" + timeStamp)
+    countTweetText = "Unique AC 数ランキング TOP " + str(countRankNum) + " !!\n"
+    api.update_with_media(filename = "data/countRankingImg_fixed.jpg", status = countTweetText + "\n" + timeStamp)
+    pointTweetText = "Point Sum ランキング TOP " + str(pointRankNum) + " !!\n"
+    api.update_with_media(filename = "data/pointRankingImg_fixed.jpg", status = pointTweetText + "\n" + timeStamp)
