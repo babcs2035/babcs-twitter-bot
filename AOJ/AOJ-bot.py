da9e78dd24e954ba367ab55c4baf60b5ffd0acfb
Bwambocos <mijyumaru2525@outlook.jp>
2019-01-01 12:11:17 +0900

    AOJ-develop: AC 検出を 1 分ごとに変更

--- AOJ/AOJ-bot.py
+++ AOJ/AOJ-bot.py
@@ -8,8 +8,8 @@ import ranking
 # インスタンス化
 sched = BlockingScheduler(job_defaults = {'max_instances' : 5})
 
-# AOJ AC 検出（15 秒ごと）
-@sched.scheduled_job('interval', seconds = 15)
+# AOJ AC 検出（1 分ごと）
+@sched.scheduled_job('interval', minutes = 1)
 def scheduled_job():
     
     print("AOJ-bot: ----- AOJ-detection Start -----")
