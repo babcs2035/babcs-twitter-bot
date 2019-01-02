bf0175ed6ae2dfff9c5b0466e5ffa9b05882a177
Bwambocos <mijyumaru2525@outlook.jp>
2019-01-02 10:13:55 +0900

    CF-develop: Bot 処理に CF-detection を追加

--- CF/CF-bot.py
+++ CF/CF-bot.py
@@ -3,6 +3,7 @@ import os
 import datetime
 from apscheduler.schedulers.blocking import BlockingScheduler
 import contest
+import detection
 
 # インスタンス化
 sched = BlockingScheduler(job_defaults = {'max_instances' : 5})
@@ -14,6 +15,14 @@ def scheduled_job():
     print("CF-bot: ----- CF-contest Start -----")
     contest.contest()
     print("CF-bot: ----- CF-contest End -----")
+
+# Codeforces AC 検出（毎時 10, 25, 40, 55 分）
+@sched.scheduled_job('cron', minute = '10, 25, 40, 55', hour = '*/1')
+def scheduled_job():
+
+    print("CF-bot: ----- CF-detection Start -----")
+    detection.detection()
+    print("CF-bot: ----- CF-detection End -----")
 
 # おまじない
 sched.start()
