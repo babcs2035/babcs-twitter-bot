56e20564d0d7855c87e7417f2554cca8fb52edf5
Bwambocos <mijyumaru2525@outlook.jp>
2019-01-11 20:14:07 +0900

    CF-develop: Bot 処理に CF-ranking を追加

--- CF/CF-bot.py
+++ CF/CF-bot.py
@@ -4,6 +4,7 @@ import datetime
 from apscheduler.schedulers.blocking import BlockingScheduler
 import contest
 import detection
+import ranking
 
 # インスタンス化
 sched = BlockingScheduler(job_defaults = {'max_instances' : 5})
@@ -23,6 +24,14 @@ def scheduled_job():
     print("CF-bot: ----- CF-detection Start -----")
     detection.detection()
     print("CF-bot: ----- CF-detection End -----")
+
+# Codeforces AC 提出数ランキング（毎日 0:00）
+@sched.scheduled_job('cron', minute = '0', hour = '0')
+def scheduled_job():
+
+    print("CF-bot: ----- CF-ranking Start -----")
+    ranking.ranking()
+    print("CF-bot: ----- CF-ranking End -----")
 
 # おまじない
 sched.start()
