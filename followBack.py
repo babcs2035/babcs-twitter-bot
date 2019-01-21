830fdbcbdda8753dbdb826ac0ef35ebcc4305772
Bwambocos <mijyumaru2525@outlook.jp>
2019-01-21 21:11:38 +0900

    followBack: フォロバした人をツイートするよう実装

--- followBack.py
+++ followBack.py
@@ -2,6 +2,8 @@
 import os
 import tweepy
 import datetime
+from requests_oauthlib import OAuth1Session
+import json
 
 def followBack():
 
@@ -22,14 +24,22 @@ def followBack():
     set_apr = set(followers) - set(friends)
     list_apr = list(set_apr)
     followedCnt = 0
+    followedList = []
     for user in list_apr:
         try:
             api.create_friendship(user)
             print("followBack: Created friendship with %s" % user) 
             followedCnt = followedCnt + 1
+            # OAuth でツイートを取得
+            api_OAuth = OAuth1Session(CK, CS, AT, AS)
+            user_json = api_OAuth.get("https://api.twitter.com/1.1/users/show.json?user_id=" + str(user))
+            user_data = json.loads(user_json.text)
+            followedList.append({"user_name" : user_data["name"], "user_id" : user_data["screen_name"]})
         except tweepy.error.TweepError:
             print("followBack: Could not create friendship with %s" % user)
-    followStr = "新しく " + str(followedCnt) + " 人をフォローしました！"
+    followStr = "新しく " + str(followedCnt) + " 人をフォローしました：\n"
+    for user in followedList:
+        followStr += str(user["user_name"]) + " ( @" + str(user["user_id"]) + " )\n"
 
     # 時刻表示を作成
     timeStamp = datetime.datetime.today()
