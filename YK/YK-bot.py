a09e3ba0c14fdc6149d5f1f5547f673d1d35c928
Bwambocos <mijyumaru2525@outlook.jp>
2019-01-23 20:04:07 +0900

    YK-develop: Bot.py 追加

---
+++ YK/YK-bot.py
@@ -0,0 +1,10 @@
+﻿# import
+import os
+import datetime
+from apscheduler.schedulers.blocking import BlockingScheduler
+
+# インスタンス化
+sched = BlockingScheduler(job_defaults = {'max_instances' : 5})
+ 
+# おまじない
+sched.start()
