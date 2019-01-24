0a32f3be3b5638ea919a0ca2dee58fbbb975d318
Bwambocos <mijyumaru2525@outlook.jp>
2019-01-24 16:41:36 +0900

    YK-develop: Bot 処理に YK-ranking を追加

--- YK/YK-bot.py
+++ YK/YK-bot.py
@@ -3,6 +3,7 @@ import os
 import datetime
 from apscheduler.schedulers.blocking import BlockingScheduler
 import detection
+import ranking
 
 # インスタンス化
 sched = BlockingScheduler(job_defaults = {'max_instances' : 5})
@@ -14,6 +15,14 @@ def scheduled_job():
     print("YK-bot: ----- YK-detection Start -----")
     detection.detection()
     print("YK-bot: ----- YK-detection End -----")
+
+# yukicoder Unique AC 数ランキング（毎日 0:00）
+@sched.scheduled_job('cron', minute = '0', hour = '0')
+def scheduled_job():
+    
+    print("YK-bot: ----- YK-ranking Start -----")
+    ranking.ranking()
+    print("YK-bot: ----- YK-ranking End -----")
 
 # おまじない
 sched.start()
