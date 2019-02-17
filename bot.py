fa424d126df93f1ed5b9c00056d59175afdb50c7
Bwambocos <mijyumaru2525@outlook.jp>
2019-02-17 22:54:16 +0900

    master: NewRelic 関連を削除

--- bot.py
+++ bot.py
@@ -5,39 +5,33 @@ import datetime
 from apscheduler.schedulers.blocking import BlockingScheduler
 import announce
 import followBack
-import newrelic.agent
 
-@newrelic.agent.wsgi_application()
-def application(environ, start_response):
+# インスタンス化
+sched = BlockingScheduler(job_defaults = {'max_instances' : 5})
 
-    # インスタンス化
-    sched = BlockingScheduler(job_defaults = {'max_instances' : 5})
-    
-    # 定期ツイート（毎時 30 分）
-    @sched.scheduled_job('cron', minute = '30', hour = '*/1')
-    def scheduled_job():
-    
-        print("bot: ----- announce Start -----")
-        # announce.announce()
-        print("bot: announce has been passed")
-        print("bot: ----- announce End -----")
-        
-    # フォロバ（毎時 0, 20, 40 分）
-    @sched.scheduled_job('cron', minute = '0, 20, 40', hour = '*/1')
-    def scheduled_job():
-    
-        print("bot: ----- followBack Start -----")
-        followBack.followBack()
-        print("bot: ----- followBack End -----")
-    
-    # 各種 Bot を呼び出す
-    subprocess.Popen(["python", "twitter.py"])
-    subprocess.Popen(["python", "AtCoder/AtCoder-bot.py"])
-    subprocess.Popen(["python", "AOJ/AOJ-bot.py"])
-    subprocess.Popen(["python", "CF/CF-bot.py"])
-    subprocess.Popen(["python", "YK/YK-bot.py"])
-    
-    # おまじない
-    sched.start()
+# 定期ツイート（毎時 30 分）
+@sched.scheduled_job('cron', minute = '30', hour = '*/1')
+def scheduled_job():
 
-application(os.environ, True)
+    print("bot: ----- announce Start -----")
+    # announce.announce()
+    print("bot: announce has been passed")
+    print("bot: ----- announce End -----")
+    
+# フォロバ（毎時 0, 20, 40 分）
+@sched.scheduled_job('cron', minute = '0, 20, 40', hour = '*/1')
+def scheduled_job():
+
+    print("bot: ----- followBack Start -----")
+    followBack.followBack()
+    print("bot: ----- followBack End -----")
+
+# 各種 Bot を呼び出す
+subprocess.Popen(["python", "twitter.py"])
+subprocess.Popen(["python", "AtCoder/AtCoder-bot.py"])
+subprocess.Popen(["python", "AOJ/AOJ-bot.py"])
+subprocess.Popen(["python", "CF/CF-bot.py"])
+subprocess.Popen(["python", "YK/YK-bot.py"])
+
+# おまじない
+sched.start()
