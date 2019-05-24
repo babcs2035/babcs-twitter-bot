98a1690f9e53c08c26c7457d4d869596de46a43e
Bwambocos <mijyumaru2525@outlook.jp>
2019-05-24 20:24:56 +0900

    AtCoder-detection: WJ の提出を無視してしまう問題を修正

--- AtCoder/detection.py
+++ AtCoder/detection.py
@@ -181,22 +181,25 @@ def detection(type):
     print("AtCoder-detection: Downloaded contestsJsonData")
     
     checkContests = []
-    border = datetime.datetime.today() - datetime.timedelta(14)
+    border = datetime.datetime.today() - datetime.timedelta(30)
     for contest in contestsJsonData:
         date = epoch_to_datetime(contest["startTimeSeconds"] + contest["durationSeconds"])
         if type == 0:
-            if not (border <= date and date <= datetime.datetime.today()):
+            if date < border:
                 checkContests.append(contest)
+            else:
+                print(contest["id"])
         if type == 1:
             if border <= date and date <= datetime.datetime.today():
                 checkContests.append(contest)
 
+    newLastSubID = {}
     for contest in checkContests:
 
         # ページ送り
         sublistPageNum = 1
         subCount = 0
-        newLastSubID = -1
+        newLastSubID[str(contest["id"])] = -1
 
         while True:
             sublistURL = "https://atcoder.jp/contests/" + str(contest["id"]) + "/submissions?page=" + str(sublistPageNum)
@@ -216,11 +219,16 @@ def detection(type):
             del sublistRows[0]
             skipFlag = False
             for row in sublistRows:
+
+                subData = [cell.get_text() for cell in row.select("td")]
+                if subData[6] == "WJ" or ('0' <= subData[6][0] and subData[6][0] <= '9'):
+                    continue
+
                 links = row.find_all("a")
                 subID = int(str(links[3].get("href")).split("/")[4])
                 userID = str(links[1].get("href")).split("/")[2]
-                if newLastSubID == -1:
-                    newLastSubID = subID
+                if newLastSubID[str(contest["id"])] == -1:
+                    newLastSubID[str(contest["id"])] = subID
                 if str(contest["id"]) not in lastSubID:
                     skipFlag = True
                     break
@@ -230,7 +238,6 @@ def detection(type):
                 subCount = subCount + 1
 
                 # ユーザーの AC 提出かどうか判定
-                subData = [cell.get_text() for cell in row.select("td")]
                 if subData[6] == "AC":
                     idx = 0
                     for ids in AtCoderID:
@@ -240,31 +247,7 @@ def detection(type):
                                     break
                             else:
                                 noticeFlag[str(ids)] = "on"
-                            # score = int(float(str(subData[4])))
-                            # imagePath = "AtCoder/data/detection/"
-                            # if score <= 100:
-                            #     imagePath = imagePath + "100"
-                            # elif score <= 200:
-                            #     imagePath = imagePath + "200"
-                            # elif score <= 300:
-                            #     imagePath = imagePath + "300"
-                            # elif score <= 400:
-                            #     imagePath = imagePath + "400"
-                            # elif score <= 600:
-                            #     imagePath = imagePath + "600"
-                            # elif score <= 800:
-                            #     imagePath = imagePath + "800"
-                            # elif score <= 1100:
-                            #     imagePath = imagePath + "1100"
-                            # elif score <= 1500:
-                            #     imagePath = imagePath + "1500"
-                            # elif score <= 1900:
-                            #     imagePath = imagePath + "1900"
-                            # else:
-                            #     imagePath = imagePath + "2000"
-                            # imagePath = imagePath + ".png"
                             try:
-                                # api.update_with_media(filename = imagePath, status = userID + " ( @" + TwitterID[idx] + " ) さんが " + str(contest["title"]) + "：" + str(subData[1]) + " を AC しました！\n提出ソースコード：" + "https://atcoder.jp" + str(links[3].get("href")) + "\n" + timeStamp)
                                 api.update_status(userID + " ( @" + TwitterID[idx] + " ) さんが <AtCoder> " + str(contest["title"]) + "：" + str(subData[1]) + " を AC しました！\nhttps://atcoder.jp" + str(links[3].get("href")) + "\n" + timeStamp)
                                 print("AtCoder-detection: " + userID + " ( @" + TwitterID[idx] + " ) 's new AC submission (contest : " + str(contest["title"]) + ", problem : " + str(subData[1]) + ")")
                             except:
@@ -274,15 +257,13 @@ def detection(type):
                 break
             sublistPageNum = sublistPageNum + 1
 
-        # lastSubID を更新
-        lastSubID[str(contest["id"])] = newLastSubID
-        print("AtCoder-detection: Checked " + contest["title"] + " submissions (subCount : " + str(subCount) + ", newlastSubID : " + str(lastSubID[str(contest["id"])]) + ")")
+        print("AtCoder-detection: Checked " + contest["title"] + " submissions (subCount : " + str(subCount) + ", newlastSubID : " + str(newLastSubID[str(contest["id"])]) + ")")
 
     # データをアップロード
     if type == 0:
-        lastSubID_All = lastSubID
+        lastSubID_All = newLastSubID
     if type == 1:
-        lastSubID_Recent = lastSubID
+        lastSubID_Recent = newLastSubID
     uploadToDropbox(type)
 
 if __name__ == '__main__':
