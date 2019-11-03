e28f9e2ed889efe7be403b47370aaa2d8f5b79a3
Bwambocos <mijyumaru2525@outlook.jp>
2019-11-03 17:10:16 +0900

    master: main.py にログ出力を追加

--- main.py
+++ main.py
@@ -3,4 +3,6 @@ import subprocess
 
 # 各 Bot を開始
 subprocess.Popen(["python", "cper_bot/cper_bot.py"])
+print("main: started cper_bot")
 subprocess.Popen(["python", "cpcontest_bot/cpcontest_bot.py"])
+print("main: started cpcontest_bot")
