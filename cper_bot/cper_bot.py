6371b8af512fddc588340d35e1ffe4252c2c9211
Bwambocos <mijyumaru2525@outlook.jp>
2019-11-06 21:20:15 +0900

    master: main.py を bot.py に名称変更

--- cper_bot/cper_bot.py
+++ cper_bot/cper_bot.py
@@ -15,12 +15,12 @@ def scheduled_job():
     followBack.followBack()
     print("cper_bot: ----- followBack End -----")
     
+# 各 Bot を呼び出し
 subprocess.Popen(["python", "cper_bot/twitter.py"])
 subprocess.Popen(["python", "cper_bot/AtCoder/AtCoder-bot.py"])
 subprocess.Popen(["python", "cper_bot/AOJ/AOJ-bot.py"])
 subprocess.Popen(["python", "cper_bot/CF/CF-bot.py"])
 subprocess.Popen(["python", "cper_bot/YK/YK-bot.py"])
-subprocess.Popen(["python", "cpcontest_bot/cpcontest_bot.py"])
 
 # おまじない
 sched.start()
