080e8e075aeadf8a0245fa7e7542c6fc2ab9bd75
Bwambocos <mijyumaru2525@outlook.jp>
2019-12-16 16:34:16 +0900

    cpcontest_bot: ID 登録者は無条件にランキング浮上ツイートをするよう実装

--- cpcontest_bot/cpcontest_bot.py
+++ cpcontest_bot/cpcontest_bot.py
@@ -6,7 +6,7 @@ import followBack
 import getLiveContestID
 import FA
 import updateHighestScore
-import top20
+import ranking
 
 # インスタンス化
 sched = BlockingScheduler(job_defaults = {'max_instances' : 10})
@@ -49,9 +49,9 @@ def scheduled_job():
         print("cpcontest_bot: ----- updateHighestScore End -----")
 
         # 20 位以内に浮上したユーザーを検知
-        print("cpcontest_bot: ----- top20 Start -----")
-        top20.top20(liveContestIDs)
-        print("cpcontest_bot: ----- top20 End -----")
+        print("cpcontest_bot: ----- ranking Start -----")
+        ranking.ranking(liveContestIDs)
+        print("cpcontest_bot: ----- ranking End -----")
 
 # おまじない
 sched.start()
