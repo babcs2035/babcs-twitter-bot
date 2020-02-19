9664e3247a2dab3779d63f8b5b496800905de63a
Bwambocos <mijyumaru2525@outlook.jp>
2020-02-19 23:03:10 +0900

    YK-develop: BlockingScheduler の設定を変更

--- YK/YK-bot.py
+++ YK/YK-bot.py
@@ -2,15 +2,21 @@
 import os
 import datetime
 from apscheduler.schedulers.blocking import BlockingScheduler
+from apscheduler.executors.pool import ThreadPoolExecutor, ProcessPoolExecutor
 import detection
 import ranking
 import contest
 
 # インスタンス化
-sched = BlockingScheduler(job_defaults = {'max_instances' : 10})
+sched = BlockingScheduler(
+    executors = {
+        'threadpool' : ThreadPoolExecutor(max_workers = 2),
+        'processpool' : ProcessPoolExecutor(max_workers = 1)
+    }
+)
 
 # yukicoder AC 検出（1 分ごと）
-@sched.scheduled_job('interval', minutes = 1)
+@sched.scheduled_job('interval', minutes = 1, executor = 'threadpool')
 def scheduled_job():
     
     print("cper_bot-YK-bot: ----- YK-detection Start -----")
@@ -18,7 +24,7 @@ def scheduled_job():
     print("cper_bot-YK-bot: ----- YK-detection End -----")
 
 # yukicoder Unique AC 数ランキング（毎日 0:00）
-@sched.scheduled_job('cron', minute = '0', hour = '0')
+@sched.scheduled_job('cron', minute = '0', hour = '0', executor = 'threadpool')
 def scheduled_job():
     
     print("cper_bot-YK-bot: ----- YK-ranking Start -----")
@@ -26,7 +32,7 @@ def scheduled_job():
     print("cper_bot-YK-bot: ----- YK-ranking End -----")
 
 # yukicoder コンテスト予定（毎日 0, 6, 12, 18 時）
-@sched.scheduled_job('cron', minute = '0', hour = '0, 6, 12, 18')
+@sched.scheduled_job('cron', minute = '0', hour = '0, 6, 12, 18', executor = 'threadpool')
 def scheduled_job():
     
     print("cper_bot-YK-bot: ----- YK-contest Start -----")
