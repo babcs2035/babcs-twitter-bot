eeb602eee2300c936b2c355c2cb8224fd2de513c
Bwambocos <mijyumaru2525@outlook.jp>
2022-03-16 18:19:57 +0900

    AOJ: Fixed tweepy uses

--- AOJ/ranking.py
+++ AOJ/ranking.py
@@ -127,7 +127,7 @@ def ranking():
 
     # ランキングをツイート
     countTweetText = "AOJ Unique AC 数ランキング TOP " + str(countRankNum) + "\n入賞の " + " , ".join(awardsList) + " さん おめでとうございます！\n"
-    api.update_with_media(filename = "AOJ/data/countRankingImg_fixed.jpg", status = countTweetText + "\n" + timeStamp)
+    api.update_staus_with_media(filename = "AOJ/data/countRankingImg_fixed.jpg", status = countTweetText + "\n" + timeStamp)
     
     # データをアップロード
     acCount = nowACCount
