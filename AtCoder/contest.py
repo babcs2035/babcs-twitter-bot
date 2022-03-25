99aa27f20c2097bc0d2ba9280614d12830aee54a
Bwambocos <mijyumaru2525@outlook.jp>
2022-03-25 16:50:20 +0900

    AtCoder: Fix contest.py

--- AtCoder/contest.py
+++ AtCoder/contest.py
@@ -17,26 +17,21 @@ def epoch_to_datetime(epoch):
 def sec_to_time(sec):
     return "{0.hours:02}:{0.minutes:02}".format(relativedelta(seconds=sec))
 
+def isSponseredContest(str):
+    return (str.find("（") != -1 or str.find("(") != -1)
+
 def contest():
     
-    # 各種キー設定
-    CK = os.environ["CONSUMER_KEY"]
-    CS = os.environ["CONSUMER_SECRET"]
-    AT = os.environ["ACCESS_TOKEN_KEY"]
-    AS = os.environ["ACCESS_TOKEN_SECRET"]
-    
-    # Twitter オブジェクトの生成
-    auth = tweepy.OAuthHandler(CK, CS)
-    auth.set_access_token(AT, AS)
+    # Create Twitter Object
+    auth = tweepy.OAuthHandler(os.environ["CONSUMER_KEY"], os.environ["CONSUMER_SECRET"])
+    auth.set_access_token(os.environ["ACCESS_TOKEN_KEY"], os.environ["ACCESS_TOKEN_SECRET"])
     api = tweepy.API(auth)
 
-    # 時刻表示を作成
-    timeStamp = datetime.datetime.today()
-    timeStamp = str(timeStamp.strftime("%Y/%m/%d %H:%M"))
+    # Create time stamp
+    timeStamp = str(datetime.datetime.today().strftime("%Y/%m/%d %H:%M"))
 
-    # 開催予定のコンテストを取得
-    contestsURL = "https://atcoder.jp/contests/?lang=ja"
-    contestsHTML = requests.get(contestsURL)
+    # Get Upcoming contests list
+    contestsHTML = requests.get("https://atcoder.jp/contests/?lang=ja")
     try:
         contestsHTML.raise_for_status()
         contestsData = BeautifulSoup(contestsHTML.text, "html.parser")
@@ -48,64 +43,68 @@ def contest():
         api.update_status("現在予定されている AtCoder コンテストはありません．\nhttps://atcoder.jp/contests/\n\n" + timeStamp)
         return
 
-    # １行ずつ解析
+    # Analyze list
     contestsRows = contestsTable[1].find_all("tr")    
     del contestsRows[0]
-    print("cper_bot-AtCoder-contest: Downloaded contestsRows")
     contestsList = []
+    sponseredContestsCnt = 0
     for contest in contestsRows:
         contestData = [contest.get_text() for contest in contest.select("td")]
         contestData[0] = contestData[0][:-5]
-        contestData[1] = contestData[1][3:]
+        contestData[1] = contestData[1].replace("◉", "")
+        contestData[1] = contestData[1].replace("\n", "")
+        contestData[1] = contestData[1].replace("\t", "")
+        contestData[1] = contestData[1][0:1] + " " + contestData[1][1:]
+        contestData[1] = contestData[1].replace("（", "\n   （")
+        contestData[1] = contestData[1].replace("(", "\n      (")
+        contestData.append(isSponseredContest(contestData[1]))
         contestsList.append(contestData)
+        sponseredContestsCnt += contestData[4]
 
-    # 画像生成
-    listFontR = ImageFont.truetype("AtCoder/data/fontR.ttc", 32)
-    listFontB = ImageFont.truetype("AtCoder/data/fontB.ttc", 32)
-    contestsListFirstImg = Image.open("AtCoder/data/contestsListImg (first).jpg")
-    contestsListImg = Image.new("RGB", (1852, 68 + 64 * len(contestsList)))
-    contestsListImg.paste(contestsListFirstImg, (0, 0))
+    # Generate image
+    listFontR = ImageFont.truetype("fontR.ttc", 32)
+    listFontB = ImageFont.truetype("fontB.ttc", 32)
+    contestsListTitleImg = Image.open("AtCoder/data/contestsListImg (title).jpg")
+    contestsListImg = Image.new("RGB", (1852, 68 + 64 * (len(contestsList) + sponseredContestsCnt)))
+    contestsListImg.paste(contestsListTitleImg, (0, 0))
     idx = 0
     for contest in contestsList:
-        contest[1] = contest[1].replace("◉", "")
-        contest[1] = contest[1].replace("\n", "")
-        contest[1] = contest[1].replace("\t", "")
-        contestListImg = Image.open("AtCoder/data/contestsListImg (cell).jpg")
+        imgPath = "AtCoder/data/contestsListImg (normal).jpg"
+        if contest[4]:
+            imgPath = "AtCoder/data/contestsListImg (sponsered).jpg"
+        contestListImg = Image.open(imgPath)
         contestListDraw = ImageDraw.Draw(contestListImg)
-        contestListDraw.text((10, 7), str(contest[0]), fill = (0, 0, 0), font = listFontR)
+        contestListDraw.text((10, 7 + 32 * contest[4]), str(contest[0]), fill = (0, 0, 0), font = listFontR)
+        specialFontColor = (0, 0, 0)
         if str(contest[3]).find("~ 1999") != -1:
-            contestListDraw.text((360, 7), str(contest[1]), fill = (0, 0, 255), font = listFontB)
-            contestListDraw.text((1660, 7), str(contest[3]), fill = (0, 0, 255), font = listFontB)
+            specialFontColor = (0, 0, 255)
         elif str(contest[3]).find("~ 2799") != -1:
-            contestListDraw.text((360, 7), str(contest[1]), fill = (255, 150, 50), font = listFontB)
-            contestListDraw.text((1660, 7), str(contest[3]), fill = (255, 150, 50), font = listFontB)
+            specialFontColor = (255, 150, 50)
         elif str(contest[3]).find("1200 ~") != -1 or str(contest[3]).find("2000 ~") != -1:
-            contestListDraw.text((360, 7), str(contest[1]), fill = (255, 0, 0), font = listFontB)
-            contestListDraw.text((1660, 7), str(contest[3]), fill = (255, 0, 0), font = listFontB)
-        else:
-            contestListDraw.text((360, 7), str(contest[1]), fill = (0, 0, 0), font = listFontR)
-            contestListDraw.text((1660, 7), str(contest[3]), fill = (0, 0, 0), font = listFontR)
-        contestListDraw.text((1460, 7), str(contest[2]), fill = (0, 0, 0), font = listFontR)
+            specialFontColor = (255, 0, 0)
+        contestListDraw.text((360, 7 + 7 * contest[4]), str(contest[1]), fill = specialFontColor, font = listFontB)
+        contestListDraw.text((1460, 7 + 32 * contest[4]), str(contest[2]), fill = (0, 0, 0), font = listFontR)
+        contestListDraw.text((1660, 7 + 32 * contest[4]), str(contest[3]), fill = specialFontColor, font = listFontB)
         contestsListImg.paste(contestListImg, (0, 68 + 64 * idx))
-        idx = idx + 1
+        idx += (1 + contest[4])
 
-        # メモリ解放
+        # Release memory
         del contestListImg
         del contestListDraw
         gc.collect()
 
     contestsListImg.save("AtCoder/contestsListImg_fixed.jpg")
 
-    # リストをツイート
+    # Tweet
     listTweetText = "現在 " + str(len(contestsList)) + " 件の AtCoder コンテストが予定されています．\nhttps://atcoder.jp/contests/\n"
     api.update_status_with_media(filename = "AtCoder/contestsListImg_fixed.jpg", status = listTweetText + "\n" + timeStamp)
     print("cper_bot-AtCoder-contest: Tweeted contestsListImg_fixed")
 
-    # メモリ解放
+    # Release memory
     del contestsList
     del listFontR
     del listFontB
-    del contestsListFirstImg
+    del contestsListTitleImg
     del contestsListImg
     gc.collect()
 
